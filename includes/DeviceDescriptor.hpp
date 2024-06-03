fe05867053a5cf0bd72e7cb147cf2fe7b94513ed
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2024-06-03 12:50:41 +0200

    refactor DeviceIdentity struct to use correct *_id types, add device_name field, improve thrown exceptions from DeviceDescriptor ctor

--- includes/DeviceDescriptor.hpp
+++ includes/DeviceDescriptor.hpp
@@ -24,21 +24,32 @@ inline std::string makeDeviceIdentity(
 }
 
 struct DeviceIdentity {
-  const std::string vendor_id;
+  const uint16_t vendor_id;
   const std::string vendor_name;
-  const std::string device_id;
+  const uint32_t device_id;
+  const TextID device_name;
 
-  DeviceIdentity(const std::string& _vendor_id, const std::string& _vendor_name,
-      const std::string& _device_id) {
-    if (vendor_id.empty()) {
-      throw std::invalid_argument("Vendor ID can not be empty");
+  DeviceIdentity(uint16_t _vendor_id, const std::string& _vendor_name,
+      uint32_t _device_id, const TextID& _device_name)
+      : vendor_id(_vendor_id), vendor_name(_vendor_name), device_id(_device_id),
+        device_name(_device_name) {
+    if (vendor_id != 0) {
+      throw std::invalid_argument(
+          "Failed to create DeviceIdentity. Vendor ID can not be empty");
     }
-    if (device_id.empty()) {
-      throw std::invalid_argument("Device ID can not be empty");
+    if (device_id != 0) {
+      throw std::invalid_argument(
+          "Failed to create DeviceIdentity. Device ID can not be empty");
+    }
+    if (!device_name) {
+      throw std::invalid_argument(
+          "Failed to create DeviceIdentity. Device name can not be empty");
     }
   }
 
-  std::string operator()() { return makeDeviceIdentity(vendor_id, device_id); }
+  std::string operator()() const {
+    return std::to_string(vendor_id) + "-" + std::to_string(device_id);
+  }
 };
 
 struct DeviceDescriptor {
@@ -47,11 +58,19 @@ struct DeviceDescriptor {
   const DeviceIdentity identity;
   const VariablesMap variables;
 
-  DeviceDescriptor(const std::string& vendor_id, const std::string& vendor_name,
-      const std::string& device_id, const VariablesMap& _variables)
-      : identity(vendor_id, vendor_name, device_id), variables(_variables) {
+  DeviceDescriptor(uint16_t vendor_id, const std::string& vendor_name,
+      uint32_t device_id, const TextID& device_name,
+      const VariablesMap& _variables)
+      : DeviceDescriptor(
+            DeviceIdentity(vendor_id, vendor_name, device_id, device_name),
+            _variables) {}
+
+  DeviceDescriptor(
+      const DeviceIdentity& _identity, const VariablesMap& _variables)
+      : identity(_identity), variables(_variables) {
     if (variables.empty()) {
-      throw std::invalid_argument("Variables can not be empty");
+      throw std::invalid_argument(
+          "Failed to create DeviceDescriptor. Variables can not be empty");
     }
   }
 };
