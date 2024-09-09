81ae097d0714346be8279432f7fd84ddab87973d
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2024-09-09 17:44:02 +0200

    add simple sanity check

--- sources/Example/main.cpp
+++ sources/Example/main.cpp
@@ -1,9 +1,16 @@
+#include "Serializer.hpp"
+
 #include <iostream>
 
 using namespace std;
+using namespace IODD;
 
 int main() {
   cout << "Hello world" << endl;
+
+  auto repo = deserializeModel("config");
+
+  cout << "IODD Repository has " << repo.size() << " descriptors" << endl;
 
   exit(EXIT_SUCCESS);
 }
