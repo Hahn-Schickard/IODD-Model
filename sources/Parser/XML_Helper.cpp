a8d30ed343881d42ab6d2f13c178e943b83d3a58
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2024-09-10 15:12:53 +0200

    add XML Helper utils

---
+++ sources/Parser/XML_Helper.cpp
@@ -0,0 +1,100 @@
+#include "XML_Helper.hpp"
+
+#include <cstring>
+#include <stdexcept>
+
+#include <sstream>
+
+using namespace std;
+using namespace pugi;
+
+xml_document getXML(const std::filesystem::path& path) {
+  if (!filesystem::exists(path)) {
+    throw runtime_error(path.string() + " does not exists");
+  }
+
+  if (path.extension() != ".xml") {
+    throw invalid_argument(path.string() + " is not an XML document");
+  }
+
+  xml_document xml;
+  if (auto status = xml.load_file(path.c_str())) {
+    return xml;
+  } else {
+    throw runtime_error("Failed to load " + path.string() +
+        " as an XML document. " + status.description());
+  }
+}
+
+xml_node getXMLNode(const string& node_name,
+    const xml_document& doc,
+    const filesystem::path& path) {
+  if (node_name.empty()) {
+    throw invalid_argument("Could not get XML Node from XML doc. Node name "
+                           "argument can not be empty");
+  }
+
+  auto node = doc.child(node_name.c_str());
+  if (!node.empty()) {
+    return node;
+  } else {
+    throw NodeNotFound(path, node_name);
+  }
+}
+
+xml_node getXMLNode(const string& node_name,
+    const xml_node& parent,
+    const optional<filesystem::path>& path) {
+  if (node_name.empty()) {
+    throw invalid_argument(
+        "Could not get XML Node from parent XML node. Node name "
+        "argument can not be empty");
+  }
+
+  auto node = parent.child(node_name.c_str());
+  if (!node.empty()) {
+    return node;
+  } else {
+    if (path.has_value()) {
+      throw NodeNotFound(path.value(), parent, node_name);
+    } else {
+      throw NodeNotFound(parent, node_name);
+    }
+  }
+}
+
+xml_node getXMLNode(const vector<string>& nodes,
+    const xml_node& parent,
+    const optional<filesystem::path>& path) {
+  if (nodes.empty()) {
+    throw invalid_argument(
+        "Could not get XML Node from parent XML node. Nodes vector<string> "
+        "argument can not be empty");
+  }
+
+  auto it = nodes.begin();
+  auto node = getXMLNode(*it, parent, path);
+  ++it;
+  while (it != nodes.end()) {
+    node = getXMLNode(*it, node, path);
+    ++it;
+  }
+
+  return node;
+}
+
+xml_attribute getXMLAttribute(
+    const string& attribute_name, const xml_node& node) {
+  if (attribute_name.empty()) {
+    throw invalid_argument("Could not get XML Node attribute " +
+        string(node.name()) +
+        " XML node. Attribute name argument can not be empty");
+  }
+
+  auto result = node.attribute(attribute_name.c_str());
+  if (!result.empty()) {
+    return result;
+  } else {
+    throw AttributeNotFound(node, attribute_name);
+  }
+}
---
+++ sources/Parser/XML_Helper.hpp
@@ -0,0 +1,53 @@
+#include <pugixml.hpp>
+
+#include <filesystem>
+#include <optional>
+#include <string>
+#include <vector>
+
+struct NodeNotFound : public std::runtime_error {
+  NodeNotFound(const pugi::xml_document& doc, const std::string& name)
+      : runtime_error(
+            std::string(doc.name()) + " does not have " + name + " xml node") {}
+
+  NodeNotFound(const std::filesystem::path& path, const std::string& name)
+      : runtime_error(path.string() + " does not have " + name + " xml node") {}
+
+  NodeNotFound(const std::filesystem::path& path,
+      const pugi::xml_node& parent,
+      const std::string& name)
+      : runtime_error(path.string() + "  Doc parent node " +
+            std::string(parent.name()) + " does not have " + name +
+            " xml node") {}
+
+  NodeNotFound(const pugi::xml_node& parent, const std::string& name)
+      : runtime_error("Parent node " + std::string(parent.name()) +
+            " does not have " + name + " xml node") {}
+};
+
+struct AttributeNotFound : public std::runtime_error {
+  AttributeNotFound(const pugi::xml_node& node, const std::string& name)
+      : runtime_error("XML Node " + std::string(node.name()) +
+            " does not have " + name + " attribute") {}
+};
+
+pugi::xml_document getXML(const std::filesystem::path& path);
+
+// assumes doc is not empty
+pugi::xml_node getXMLNode(const std::string& node_name,
+    const pugi::xml_document& doc,
+    const std::filesystem::path& path);
+
+// assumes parent is not empty
+pugi::xml_node getXMLNode(const std::string& node_name,
+    const pugi::xml_node& parent,
+    const std::optional<std::filesystem::path>& path = std::nullopt);
+
+// assumes parent is not empty
+pugi::xml_node getXMLNode(const std::vector<std::string>& nodes,
+    const pugi::xml_node& parent,
+    const std::optional<std::filesystem::path>& path = std::nullopt);
+
+// assumes node is not empty
+pugi::xml_attribute getXMLAttribute(
+    const std::string& attribute_name, const pugi::xml_node& node);
