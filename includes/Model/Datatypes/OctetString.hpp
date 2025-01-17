b04b7066da34af3ba144faf22e5a4d9630ec00d9
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-01-17 13:22:56 +0100

    add limitation for OctetStringT length

--- includes/Model/Datatypes/OctetString.hpp
+++ includes/Model/Datatypes/OctetString.hpp
@@ -2,12 +2,18 @@
 #define __IODD_STANDARD_DEFINES_OCTET_STRING_T_HPP
 
 #include <cstdint>
+#include <stdexcept>
 
 namespace IODD {
 struct OctetStringT {
   OctetStringT() = default;
 
-  OctetStringT(size_t length) : length_(length) {}
+  OctetStringT(size_t length) : length_(length) {
+    constexpr uint8_t MAX_STRING_SIZE = 232;
+    if (length > MAX_STRING_SIZE) {
+      throw std::invalid_argument("Strings can not exceed 232 bytes in length");
+    }
+  }
 
   size_t hash() const noexcept { return length_; }
 
